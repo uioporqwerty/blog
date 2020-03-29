918c4e947b0f4d05b4c2185fdb8b07f3ee5849cc
uioporqwerty <nitish.sachar@protonmail.com>
2020-03-29 03:29:26 -0700

    add google analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-52309656-5`,
       },
     },
     `gatsby-plugin-feed`,
